81aed7de8bdd4307cdabcfda5bb4dc375a213233
LubnaLukman <dhibilu@gmail.com>
2024-09-14 21:28:51 +0400

    updated

--- jobproject/jobproject/settings.py
+++ jobproject/jobproject/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'myproject',
         'USER':'root',
-        'PASSWORD':'root1234',
+        'PASSWORD':'',
         'HOST':'localhost',
         'PORT':'3306'
     }
